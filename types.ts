ebc8160760844da9a687457a01a1b99eb0f26c57
defensacivilyerbabuena-gif <defensacivilyerbabuena@gmail.com>
2025-12-26 15:00:10 -0300

    Update types.ts

--- types.ts
+++ types.ts
@@ -7,6 +7,24 @@ export interface TeamMember {
   lat: number;
   lng: number;
   distance?: string;
+  channel_id: string;
+}
+
+export interface RadioHistory {
+  id: string;
+  sender_name: string;
+  lat: number;
+  lng: number;
+  audio_data: string;
+  created_at: string;
+  channel_id: string;
+}
+
+export interface Channel {
+  id: string;
+  name: string;
+  password?: string | null;
+  created_at: string;
 }
 
 export enum ConnectionState {
